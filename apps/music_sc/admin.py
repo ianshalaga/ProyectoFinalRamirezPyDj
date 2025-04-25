e65b761b5baa784640f616b4719bb0640ae55d21
Darién Julián Ramírez <ianshalaga@gmail.com>
2025-04-25 23:06:20 +0200

    Soulcalibur music models created

--- apps/music_sc/admin.py
+++ apps/music_sc/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Game)  # admin.site.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ["order", "name"]
+    search_fields = ["name"]
+    list_filter = ["name"]
+    ordering = ["order"]
+
+
+@admin.register(models.Album)  # admin.site.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ["game", "order", "name", "type"]
+    search_fields = ["game", "name"]
+    list_filter = ["game"]
+    ordering = ["order"]
+
+
+@admin.register(models.Song)  # admin.site.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ["name", "other_name",
+                    "duration", "variation_type", "original_song"]
+    search_fields = ["name", "other_name"]
+    list_filter = ["variation_type"]
+    ordering = ["name"]
+
+
+@admin.register(models.AlbumSong)  # admin.site.register(AlbumSong)
+class AlbumSongAdmin(admin.ModelAdmin):
+    list_display = ["album", "song_number", "song"]
+    search_fields = ["song", "album"]
+    list_filter = ["album"]
+    ordering = ["song_number"]
+
+
+@admin.register(models.GameMode)  # admin.site.register(GameMode)
+class GameModeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    list_filter = ["name"]
+    ordering = ["name"]
+
+
+@admin.register(models.Stage)  # admin.site.register(Stage)
+class StageAdmin(admin.ModelAdmin):
+    list_display = ["name", "other_name"]
+    search_fields = ["name", "other_name"]
+    list_filter = ["name"]
+    ordering = ["name"]
+
+
+@admin.register(models.Character)  # admin.site.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+    list_filter = ["name"]
+    ordering = ["name"]
+
+
+@admin.register(models.Situation)  # admin.site.register(Situation)
+class SituationAdmin(admin.ModelAdmin):
+    list_display = ["description"]
+    search_fields = ["description"]
+    list_filter = ["description"]
+    ordering = ["description"]
--- apps/music_sc/models.py
+++ apps/music_sc/models.py
@@ -1,3 +1,131 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+import uuid
 
-# Create your models here.
+
+# ENUMS
+
+# class GameNameEnum(models.TextChoices):
+#     SE = "SE", _("Soul Edge")
+#     SB = "SB", _("Soul Blade")
+#     SC = "SC", _("Soulcalibur")
+#     SCII = "SCII", _("Soulcalibur II")
+#     SCIII = "SCIII", _("Soulcalibur III")
+#     SCL = "SCL", _("Soulcalibur Legends")
+#     SCIV = "SCIV", _("Soulcalibur IV")
+#     SCBD = "SCBD", _("Soulcalibur: Broken Destiny")
+#     SCV = "SCV", _("Soulcalibur V")
+#     SCVI = "SCVI", _("Soulcalibur VI")
+
+
+class VariationTypeEnum(models.TextChoices):
+    ORI = "ORI", _("Original")
+    ARR = "ARR", _("Arranged")
+    EXT = "EXT", _("Extended")
+    SHORT = "SHORT", _("Shortened")
+    FRAG = "FRAG", _("Fragment")
+
+
+class AlbumTypeEnum(models.TextChoices):
+    OST = "OST", _("Original Soundtrack")
+    GR = "GR", _("Gamerip")
+
+
+# MODELS
+
+class Game(models.Model):
+    code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    order = models.PositiveIntegerField()
+    name = models.CharField(max_length=50)  # choices=GameNameEnum.choices
+
+    def __str__(self):
+        return f"[{self.order}] {self.name}"
+
+
+class Song(models.Model):
+    code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255)
+    other_name = models.CharField(max_length=255, blank=True, null=True)
+    duration = models.DurationField()
+
+    variation_type = models.CharField(
+        max_length=6,
+        choices=VariationTypeEnum.choices,
+        default=VariationTypeEnum.ORI
+    )
+
+    original_song = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="variations"
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.duration})"
+
+
+class Album(models.Model):
+    code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    order = models.PositiveIntegerField()
+    name = models.CharField(max_length=255)
+    type = models.CharField(
+        max_length=5, choices=AlbumTypeEnum.choices, default=AlbumTypeEnum.OST)
+    game = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name="albums")
+    songs = models.ManyToManyField(
+        Song, through="AlbumSong", related_name="albums")
+
+    def __str__(self):
+        return f"[{self.game.name}] [{self.order}] {self.name} ({self.type})"
+
+
+class AlbumSong(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    song_number = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ("album", "song")
+        ordering = ["album", "song_number"]
+
+    def __str__(self):
+        return f"{self.song_number}. {self.song.name} - {self.album.name} ({self.album.get_type_display()})"
+
+
+class GameMode(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Stage(models.Model):
+    name = models.CharField(max_length=255)
+    other_name = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Situation(models.Model):
+    description = models.TextField(blank=True, null=True)
+    song = models.ForeignKey(
+        Song, on_delete=models.CASCADE, related_name="situations")
+    character = models.ForeignKey(
+        Character, null=True, blank=True, on_delete=models.SET_NULL)
+    stage = models.ForeignKey(
+        Stage, null=True, blank=True, on_delete=models.SET_NULL)
+    game_mode = models.ForeignKey(
+        GameMode, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.song.name} [{self.description}]"
